c147f6fc4c15b77ac607ece29c9d0ce3a591ab08
Frédéric MOHIER <frederic.mohier@ipmfrance.com>
2014-09-11 21:55:40 +0200

    Clean logs .... warning to debug

--- module/module.py
+++ module/module.py
@@ -24,26 +24,21 @@ def get_instance(mod_conf):
 class Checkforward(BaseModule):
     def __init__(self, mod_conf):
         BaseModule.__init__(self, mod_conf)
-        logger.info("[Checks forward] module init")
+        logger.info("[Checks forward] module initialization")
 
         try:
-            logger.debug("[Checks forward] module init : get parameters entities")
-
             # Module configuration
             self.glpi_entities = getattr(mod_conf, 'glpi_entities', '')
             self.glpi_entities = self.glpi_entities.split(',')
             if len(self.glpi_entities) > 0 and self.glpi_entities[0] == '':
                 self.glpi_entities = None
 
-            logger.warning("[Checks forward] module init : get parameters nsca")
             self.send_nsca_bin = str(getattr(mod_conf, 'send_nsca_bin', '/usr/sbin/send_nsca'))
             self.send_nsca_config = str(getattr(mod_conf, 'send_nsca_config', '/etc/send_nsca.cfg'))
 
-            logger.warning("[Checks forward] module init : get parameters nsca server")
             self.nsca_server_host = str(getattr(mod_conf, 'nsca_server_host', '127.0.0.1'))
             self.nsca_server_port = int(getattr(mod_conf, 'nsca_server_port', 5667))
 
-            logger.warning("[Checks forward] module init : log info")
             logger.info("[Checks forward] module configuration, forward to: %s:%s, using %s with configuration %s" % (self.nsca_server_host, self.nsca_server_port, self.send_nsca_bin, self.send_nsca_config))
             if self.glpi_entities:
                 logger.info("[Checks forward] module configuration, forward checks for GLPI entities: %s" % str(self.glpi_entities))
@@ -60,17 +55,13 @@ class Checkforward(BaseModule):
         logger.debug("[Checks forward] init function")
 
     def manage_initial_host_status_brok(self, b):
-        # if not self.glpi_entities:
-        #     return
-
-        # self.cache_host_entities_id[b.data['host_name']] = -1
         try:
-            logger.warning("[Checks forward] initial host status: %s" % str(b.data['customs']))
+            logger.debug("[Checks forward] initial host status: %s" % str(b.data['customs']))
             self.cache_host_entities_id[b.data['host_name']] = b.data['customs']['_ENTITIESID']
             if self.cache_host_entities_id[b.data['host_name']] in self.glpi_entities:
                 logger.info("[Checks forward] host %s checks will be forwarded (entity: %s)" % (b.data['host_name'], self.cache_host_entities_id[b.data['host_name']]))
         except:
-            logger.error("[Checks forward] Error in initial_host_status")
+            logger.warning("[Checks forward] no entity Id for host: %s" % (b.data['host_name']))
         
     def manage_host_check_result_brok(self, b):
         try:
@@ -111,7 +102,7 @@ class Checkforward(BaseModule):
             # <hostname>[TAB]<return code>[TAB]<plugin output>
             send_nsca = hostname+"\t"+str(return_code)+"\t"+output+"|"+b.data['perf_data']
 
-        logger.warning("[Checks forward] sending nsca '%s' for '%s'" % (check_type,hostname))
+        logger.debug("[Checks forward] sending nsca '%s' for '%s'" % (check_type,hostname))
         command = "/bin/echo \"%s\" | %s -H %s -p %s -c %s" % (send_nsca, self.send_nsca_bin, self.nsca_server_host, self.nsca_server_port, self.send_nsca_config)
         try:
             retcode = Popen(command, shell=True)
